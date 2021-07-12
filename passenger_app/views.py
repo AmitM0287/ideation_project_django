88b5e7e8d4a87db2c4cb4f22d6f2608e26c9ff3b
AmitM0287 <amitmanna0287@gmail.com>
2021-07-12 15:32:55 +0530

    feat: pagination added

--- passenger_app/views.py
+++ passenger_app/views.py
@@ -1,9 +1,12 @@
+import logging
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
 from logging_config.logger import get_logger
 import pandas
 from passenger_app.utils import store_data
+from passenger_app.models import passenger_movement
+from django.core.paginator import Paginator, EmptyPage
 
 # Logger configuration
 logger = get_logger()
@@ -27,10 +30,29 @@ class PassengerAPIView(APIView):
                 duplicate = store_data(date= data['Date'][index], arrivals_actual_counts= data['ArrivalsActualCounts'][index], departures_actual_counts=data['DeparturesActualCounts'][index])
             if duplicate > 0:
                 # Duplicate records found
-                return Response({'success': True, 'message': 'Database updated successfully.', 'warnings': str(duplicate) + ' duplicate values found!'}, status=status.HTTP_200_OK)
+                return Response({'success': True, 'message': 'Database updated successfully.', 'warnings': str(duplicate) + ' duplicate records found!'}, status=status.HTTP_200_OK)
             else:
                 # Data stored successfully into database
                 return Response({'success': True, 'message': 'All data successfully stored into database.'}, status=status.HTTP_200_OK)
+        except Exception as e:
+            logger.exception(e)
+            return Response({'success': False, 'message': 'Oops! Something went wrong!'}, status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request):
+        """
+            This method is used to get data from databse.
+            :param request: It's accept page_no as parameter.
+            :return: It's return response that getting data successfully from database.
+        """
+        try:
+            passenger_data = passenger_movement.objects.all()
+            paginator = Paginator(list(passenger_data.values()), 20)
+            data = paginator.page(request.data.get('page_no'))
+            # Getting data successfully from database
+            return Response({'success': True, 'message': 'Getting all data successfully from database.', "num_of_pages": paginator.num_pages, "page_no": request.data.get('page_no'), "data": data.object_list}, status=status.HTTP_200_OK)
+        except EmptyPage as e:
+            logger.exception(e)
+            return Response({'success': False, "num_of_pages": '1 to ' + str(paginator.num_pages), "page_no": request.data.get('page_no'), 'message': 'Page not found!'}, status=status.HTTP_400_BAD_REQUEST)
         except Exception as e:
             logger.exception(e)
             return Response({'success': False, 'message': 'Oops! Something went wrong!'}, status=status.HTTP_400_BAD_REQUEST)
