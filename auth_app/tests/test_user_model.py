a3c342c51689e86a148285974a1ba882ba31cb1d
AmitM0287 <amitmanna0287@gmail.com>
2021-07-16 09:38:04 +0530

    test: test cases added for user model

---
+++ auth_app/tests/test_user_model.py
@@ -0,0 +1,13 @@
+import pytest
+from mixer.backend.django import mixer
+
+pytestmark = pytest.mark.django_db
+
+
+class TestUserModel:
+    """
+        TestUserModel : Testing user model
+    """
+    def test_init(self):
+        user_obj = mixer.blend('auth_app.User')
+        assert user_obj.id == 2, 'should save an instance'
