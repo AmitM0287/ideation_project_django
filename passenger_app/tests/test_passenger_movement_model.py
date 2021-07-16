d24a1cb6d85eda0e1141cab049db05373927abc7
AmitM0287 <amitmanna0287@gmail.com>
2021-07-16 09:46:25 +0530

    test: test passenger movement model

---
+++ passenger_app/tests/test_passenger_movement_model.py
@@ -0,0 +1,13 @@
+import pytest
+from mixer.backend.django import mixer
+
+pytestmark = pytest.mark.django_db
+
+
+class TestPassengerMovementModel:
+    """
+        TestPassengerMovementModel : Testing Passenger Movement Model
+    """
+    def test_init(self):
+        passenger_obj = mixer.blend('passenger_app.passenger_movement')
+        assert passenger_obj.id == 1, 'should save an instance'
