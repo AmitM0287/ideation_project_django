33b30617d4b851f58a6d29a7b5085ee77044c71c
AmitM0287 <amitmanna0287@gmail.com>
2021-07-16 09:43:29 +0530

    feat: method added to store data into database

--- passenger_app/utils.py
+++ passenger_app/utils.py
@@ -1,28 +1,17 @@
 from passenger_app.models import passenger_movement
 
 
-# Declare count as global variable
-global count
-# Set count value to 0
-count = 0
-
 def store_data(date, arrivals_actual_counts, departures_actual_counts):
     """
         This method is used to store passenger data into database.
         :param date: It's accept date as parameter.
         :param arrivals_actual_counts: It's accept arrivals_actual_counts as parameter.
         :param departures_actual_counts: It's accept departures_actual_counts as parameter.
-        :return: It's return response that how many duplicate records found.
+        :return: None
     """
     try:
-        # Check if there is any duplicate entries or not
-        if passenger_movement.objects.filter(Date=date).exists():
-            globals()['count'] += 1
-        else:
-            passenger = passenger_movement(Date= date, ArrivalsActualCounts= arrivals_actual_counts, DeparturesActualCounts=departures_actual_counts)
-            # Save data into database
-            passenger.save()
-        # Return how many duplicate records found
-        return globals()['count']
+        passenger = passenger_movement(Date= date, ArrivalsActualCounts= arrivals_actual_counts, DeparturesActualCounts=departures_actual_counts)
+        # Save data into database
+        passenger.save()
     except Exception as e:
         raise Exception
