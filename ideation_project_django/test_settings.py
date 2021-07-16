581c615b49adb8fbd2da9567f7259e95cedf4a14
AmitM0287 <amitmanna0287@gmail.com>
2021-07-16 09:39:19 +0530

    feat: test settings added

---
+++ ideation_project_django/test_settings.py
@@ -0,0 +1,14 @@
+from ideation_project_django.settings import *
+
+
+# Database
+DATABASES = {
+    'default': {
+        'ENGINE': environ.get('DB_ENGINE'),
+        'NAME': environ.get('TEST_DB_NAME'),
+        'USER': environ.get('DB_USER'),
+        'PASSWORD': environ.get('DB_PASSWORD'),
+        'HOST': environ.get('DB_HOST'),
+        'PORT': environ.get('DB_PORT'),
+    }
+}
