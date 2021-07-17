20a3d801e3fa96ae22556f67ddd1e6c5a5e40a06
AmitM0287 <amitmanna0287@gmail.com>
2021-07-17 16:35:15 +0530

    feat: convert string to datetime

--- passenger_app/utils.py
+++ passenger_app/utils.py
@@ -1,4 +1,5 @@
 from passenger_app.models import passenger_movement
+from datetime import datetime
 
 
 def store_data(date, arrivals_actual_counts, departures_actual_counts):
@@ -10,8 +11,25 @@ def store_data(date, arrivals_actual_counts, departures_actual_counts):
         :return: None
     """
     try:
+        # Convert given date to datetime
+        date = convert_to_datetime(date)
         passenger = passenger_movement(Date= date, ArrivalsActualCounts= arrivals_actual_counts, DeparturesActualCounts=departures_actual_counts)
         # Save data into database
         passenger.save()
+    except Exception as e:
+        raise Exception
+
+
+def convert_to_datetime(string):
+    """
+        This method is used to convert string to datetime.
+        :param string: It's accept string as parameter.
+        :return: It's return datetime.
+    """
+    try:
+        # 1921M01 to 1921-01-00 00:00:00
+        string = string.replace('M', '')
+        date_time = datetime.strptime(string, '%Y%m')
+        return date_time.strftime('%Y-%m-%d %H:%M:%S')
     except Exception as e:
         raise Exception
