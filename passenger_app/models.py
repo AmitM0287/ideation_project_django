a55e05e6f03718c78016fd059ccb14292a64ec4b
AmitM0287 <amitmanna0287@gmail.com>
2021-07-17 16:34:48 +0530

    refactor: datetime field used

--- passenger_app/models.py
+++ passenger_app/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # passenger_movement model
 class passenger_movement(models.Model):
-    Date = models.CharField(max_length=20)
+    Date = models.DateTimeField()
     ArrivalsActualCounts = models.IntegerField()
     DeparturesActualCounts = models.IntegerField()
