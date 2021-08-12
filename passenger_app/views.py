20e8e07a7364e8d1716b3a1c34aee442cc9cd402
AmitM0287 <amitmanna0287@gmail.com>
2021-08-12 22:34:17 +0530

    refactor: implemented pagination using raw sql query

--- passenger_app/views.py
+++ passenger_app/views.py
@@ -1,15 +1,19 @@
+from django.db.models import query
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
 from logging_config.logger import get_logger
 import pandas
-from passenger_app.utils import store_data
+from passenger_app.utils import store_data, convert_to_list
 from passenger_app.models import passenger_movement
-from django.core.paginator import Paginator, EmptyPage
 from django.conf import settings
+from passenger_app.models import passenger_movement
 
 # Logger configuration
 logger = get_logger()
+
+class PageNotFound(Exception):
+    pass
 
 class PassengerAPIView(APIView):
     """
@@ -43,14 +47,22 @@ class PassengerAPIView(APIView):
             :return: It's return response that getting data successfully from database.
         """
         try:
-            passenger_data = passenger_movement.objects.all()
-            paginator = Paginator(list(passenger_data.values()), request.data.get('page_content') or settings.PAGE_CONTENT)
-            data = paginator.page(request.data.get('page_no') or request.GET.get('page_no'))
+            # Calculate number of pages
+            total = len(list(passenger_movement.objects.raw('SELECT id FROM passenger_app_passenger_movement')))
+            num_pages = round(total/int(settings.PAGE_CONTENT))
+            # Check given page no is exist or not
+            if int(request.GET.get('page_no')) > num_pages:
+                raise PageNotFound
+            # Calculate OFFSET value
+            offset_value = (int(request.GET.get('page_no'))-1) * settings.PAGE_CONTENT
+            # Execute query
+            for data in passenger_movement.objects.raw('SELECT * FROM passenger_app_passenger_movement LIMIT %s OFFSET %s ROWS', [settings.PAGE_CONTENT, offset_value]):
+                list_data = convert_to_list(str(data.Date)[0:10], data.ArrivalsActualCounts, data.DeparturesActualCounts)
             # Getting data successfully from database
-            return Response({'success': True, 'message': 'Getting all data successfully from database.', "num_of_pages": paginator.num_pages, "page_no": request.data.get('page_no'), "data": data.object_list}, status=status.HTTP_200_OK)
-        except EmptyPage as e:
+            return Response({'success': True, 'message': 'Getting all data successfully from database.', "num_of_pages": num_pages, "page_no": request.GET.get('page_no'), "data": list_data}, status=status.HTTP_200_OK)
+        except PageNotFound as e:
             logger.exception(e)
-            return Response({'success': False, "num_of_pages": '1 to ' + str(paginator.num_pages), "page_no": request.data.get('page_no'), 'message': 'Page not found!'}, status=status.HTTP_404_NOT_FOUND)
+            return Response({'success': False, "num_of_pages": num_pages, "page_no": request.GET.get('page_no'), 'message': 'Page not found!'}, status=status.HTTP_404_NOT_FOUND)
         except Exception as e:
             logger.exception(e)
             return Response({'success': False, 'message': 'Oops! Something went wrong!'}, status=status.HTTP_400_BAD_REQUEST)
